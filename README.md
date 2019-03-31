f9eca4493d0e5fbd35f0bddd7d73d62d1f3ae653
Chang Chu-Ming <changchuming@gmail.com>
2019-03-31 12:31:31 -0700

    Update label_image and add i2c test.

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
-https://sync-video.com/r/rxDkEMek
+# C++ Cross-compilation of Tflite Binaries
 sudo modprobe bcm2835-v4l2
 change the model name in label_image
 https://github.com/huaxiaozhong1/tensorflow-lite-apps-on-raspberry-pi-3
+
+# Face Detection Model using WIDERFACE
 https://github.com/yeephycho/tensorflow-face-detection
 
-# Model zoo
+# Tensorflow Official Model Zoo
 https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
 
-# Converting model
+# Converting Model to .tflite format
 https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/running_on_mobile_tensorflowlite.md
 
 toco --graph_def_file=$OUTPUT_DIR/tflite_graph.pb --output_file=$OUTPUT_DIR/detect.tflite --input_shapes=1,320,320,3 --input_arrays=normalized_input_image_tensor --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3' --inference_type=QUANTIZED_UINT8 --mean_values=128 --std_dev_values=128 --change_concat_input_ranges=false --allow_custom_ops
+
+# Google Object Detection API
+https://stackoverflow.com/questions/48915003/get-the-bounding-box-coordinates-in-the-tensorflow-object-detection-api-tutorial
+
+Outputs are detection_boxes, detection_classes, detection_scores, and num_detections.
+
+detection_boxes are [ymin, xmin, ymax, xmax].
+
+# Pre-built Tensorflow Binary with Tflite, Multithreading on Armv7l (Rpi 2, 3, 3+)
+Pre-built tensorflow binary with lite
+https://github.com/PINTO0309/Tensorflow-bin
+
+# Issue Talking About Tflite on Rpi (Multithreading):
+https://github.com/tensorflow/tensorflow/issues/21574
+
+# Tensorflow on RPI Original (Deprecated):
+https://github.com/samjabrahams/tensorflow-on-raspberry-pi/blob/master/GUIDE.md
+
+# Cross Compiling Tensorflow for Armv6l (Rpi A, A+, Zero) with Multithreading:
+https://www.tensorflow.org/install/source_rpi
+git clone https://github.com/tensorflow/tensorflow.git
+cd tensorflow
+git fetch origin pull/25748/head:multithread
+git checkout multithread
+
+CI_DOCKER_EXTRA_PARAMS="-e CI_BUILD_PYTHON=python3 -e CROSSTOOL_PYTHON_INCLUDE_PATH=/usr/include/python3.4" \
+    tensorflow/tools/ci_build/ci_build.sh PI-PYTHON3 \
+    tensorflow/tools/ci_build/pi/build_raspberry_pi.sh PI ONE
+
+# I2C Communication between Rpi and Arduino:
+https://dzone.com/articles/arduino-and-raspberry-pi-working-together-part-2-now-with-i2
--- python/label_image.py
+++ python/label_image.py
@@ -1,26 +1,30 @@
 import argparse
 import numpy as np
 import time
-
+import io
+import picamera
 from PIL import Image
 
 from tensorflow.contrib.lite.python import interpreter as interpreter_wrapper
-def load_labels(filename):
-  my_labels = []
-  input_file = open(filename, 'r')
-  for l in input_file:
-    my_labels.append(l.strip())
-  return my_labels
+
+def center(bounding_box):
+  ymin = bounding_box[0]
+  xmin = bounding_box[1]
+  ymax = bounding_box[2]
+  xmax = bounding_box[3]
+  ymid = ymin/2+ymax/2
+  xmid = xmin/2+xmax/2
+  return np.array((xmid, ymid))
+
+def l2_dist(v, o):
+  return np.linalg.norm(v-o)
+
 if __name__ == "__main__":
   floating_model = False
   parser = argparse.ArgumentParser()
-  parser.add_argument("-i", "--image", default="/tmp/grace_hopper.bmp", \
-    help="image to be classified")
   parser.add_argument("-m", "--model_file", \
     default="/tmp/mobilenet_v1_1.0_224_quant.tflite", \
     help=".tflite model to be executed")
-  parser.add_argument("-l", "--label_file", default="/tmp/labels.txt", \
-    help="name of file containing labels")
   parser.add_argument("--input_mean", default=127.5, help="input_mean")
   parser.add_argument("--input_std", default=127.5, \
     help="input standard deviation")
@@ -37,28 +41,52 @@ if __name__ == "__main__":
   # NxHxWxC, H:1, W:2
   height = input_details[0]['shape'][1]
   width = input_details[0]['shape'][2]
-  img = Image.open(args.image)
-  img = img.resize((width, height))
-  # add N dim
-  input_data = np.expand_dims(img, axis=0)
-  if floating_model:
-    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
 
-  interpreter.set_num_threads(int(args.num_threads))
-  interpreter.set_tensor(input_details[0]['index'], input_data)
+  camera = picamera.PiCamera()
+  try:
+    while True:
+      stream = io.BytesIO()
+      camera.capture(stream, format='jpeg')
+      # "Rewind" the stream to the beginning so we can read its content
+      stream.seek(0)
+      image = Image.open(stream)
+      image = image.resize((width, height))
+      image.save('image.jpeg')
 
-  start_time = time.time()
-  interpreter.invoke()
-  stop_time = time.time()
+      # add N dim
+      input_data = np.expand_dims(image, axis=0)
+      data = np.float32(input_data)
+      if floating_model:
+        input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+      interpreter.set_num_threads(int(args.num_threads))
+      interpreter.set_tensor(input_details[0]['index'], input_data)
+      start_time = time.time()
+      interpreter.invoke()
+      stop_time = time.time()
+      print("time: ", stop_time - start_time)
 
-  output_data = interpreter.get_tensor(output_details[0]['index'])
-  results = np.squeeze(output_data)
-  top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(args.label_file)
-  for i in top_k:
-    if floating_model:
-      print('{0:08.6f}'.format(float(results[i]))+":", labels[i])
-    else:
-      print('{0:08.6f}'.format(float(results[i]/255.0))+":", labels[i])
+      bounding_boxes = interpreter.get_tensor(output_details[0]['index'])
+      bounding_boxes = np.squeeze(bounding_boxes)
+      classes = interpreter.get_tensor(output_details[1]['index'])
+      classes = np.squeeze(classes)
+      scores =  interpreter.get_tensor(output_details[2]['index'])
+      scores = np.squeeze(scores)
+      num_real_detections = interpreter.get_tensor(output_details[3]['index'])
+      num_real_detections = int(np.squeeze(num_real_detections))
+      
+      image_center = np.array((0.5, 0.5))
+      distances = []
+      distance_vectors = []
+      for i in range(num_real_detections):
+        if scores[i] > 0.10:
+          box_center = center(bounding_boxes[i])
+          distances.append(l2_dist(box_center, image_center))
+          distance_vectors.append(np.subtract(image_center, box_center))
+      if len(distances) > 0:
+        min_dist_idx = np.argmin(distances)
+        # move_motor(distance_vectors[min_dist_idx])
+        print(distance_vectors[min_dist_idx])
 
-  print("time: ", stop_time - start_time)
+  except KeyboardInterrupt:
+      pass
+  camera.close()
